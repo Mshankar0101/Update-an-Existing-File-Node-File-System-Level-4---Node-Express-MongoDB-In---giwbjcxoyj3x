8f5e4b7a6985a57bc7775a7e2e1e669ed86623ad
mshankar0101 <madhushankar0101@gmail.com>
2024-02-20 02:22:02 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 const fs = require("fs/promises");
 const fileName = "myfile.txt";
+const fileContent = ", An online learning platform";
 const updateFile = async (fileName, fileContent) => {
   // write code here
   // dont change function name
+  return await fs.appendFile(fileName, fileContent)
   
 };
-
+updateFile(fileName, fileContent );
 module.exports = updateFile;
+
+
+// const readFile = async () => {
+//   try {
+//     const content = await fs.readFile(fileName);
+//     console.log("File content:", content.toString());
+//   } catch (error) {
+//     console.log("Error:", error.message);
+//   }
+// };
